d806745c18731d06265242e3d882e72ab457e17f
BADGUY0592 <thebadguyisbck@gmail.com>
2020-07-03 19:20:48 +0530

    bug fixes for image uploading

--- personalportfolio/models.py
+++ personalportfolio/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='personalportfolio/images/')
+    image = models.ImageField(upload_to='personalportfolio')
     urls = models.URLField(blank=True,max_length=50)
