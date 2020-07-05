43ba5f6037b8d62f45cbb889935a8ba75c84d316
BADGUY0592 <thebadguyisbck@gmail.com>
2020-07-05 19:36:34 +0530

    added blog model and view blog functionality

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+    urls = models.URLField(blank=True,max_length=50)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def blog_home(request):
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'blog/bloghome.html',{'blogs':blogs})
