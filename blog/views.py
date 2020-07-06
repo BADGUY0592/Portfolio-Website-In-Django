884e4718f7c5d1b22c1afee2dec917be0dacb92a
BADGUY0592 <thebadguyisbck@gmail.com>
2020-07-06 19:59:43 +0530

    added static functionality to the home page and dynamic urls for blogs

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def blog_home(request):
     blogs = Blog.objects.order_by('-date')[:5]
     return render(request, 'blog/bloghome.html',{'blogs':blogs})
+
+def blog_page(request,blog_id):
+    blog=get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html',{'blog':blog})
